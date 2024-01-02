39dcbee823a9f5025d70343d35ff570f3d2abf88
Abbass Tabikh <abbasstabikh87774@gmail.com>
2024-01-02 17:40:03 +0200

    finish the first row

---
+++ EwalletMobileApp/Helpers/IconFont.cs
@@ -0,0 +1,16 @@
+﻿namespace EwalletMobileApp.Helpers
+{
+    static class IconFont
+    {
+        public const string plus1 = "\ue900";
+        public const string Plus2 = "\ue902";
+        public const string Plus3 = "\ue901";
+        public const string plus4 = "\ue903";
+        public const string plus5 = "\ue904";
+        public const string Plus6 = "\uea0a";
+        public const string Trash = "\ue906";
+        public const string Home = "\ue905";
+        public const string Bin = "\ue9ac";
+        public const string Bell = "\ue951";
+    }
+}
--- EwalletMobileApp/MauiProgram.cs
+++ EwalletMobileApp/MauiProgram.cs
@@ -14,6 +14,7 @@ namespace EwalletMobileApp
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("icomoon.ttf", "Icons");
                 });
 
             builder.Services.AddSqliteConnection();
